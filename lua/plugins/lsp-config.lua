efe93cbf80a3e4b753c2b6681afc6a68c62400cd
DevMobot <pro.dev.mohit@gmail.com>
2025-01-25 17:59:34 +0530

    update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,78 +1,81 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      local util = require("lspconfig/util")
-      lspconfig.rust_analyzer.setup({
-        filetypes = { "rust" },
-        root_dir = util.root_pattern("Cargo.toml"),
-        --on_attach = function(client, bufnr)
-          --vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-          --vim.api.nvim_buf_set_option("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        --end,
-        capabilities = capabilities,
-        settings = {
-          ["rust-analyzer"] = {
-            cargo = {
-              allFeatures = true,
-            },
-          },
-        },
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
+			local lspconfig = require("lspconfig")
+			local util = require("lspconfig/util")
+			lspconfig.rust_analyzer.setup({
+				filetypes = { "rust" },
+				root_dir = util.root_pattern("Cargo.toml"),
+				--on_attach = function(client, bufnr)
+				--vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+				--vim.api.nvim_buf_set_option("n", "<leader>ca", vim.lsp.buf.code_action, {})
+				--end,
+				capabilities = capabilities,
+				settings = {
+					["rust-analyzer"] = {
+						cargo = {
+							allFeatures = true,
+						},
+					},
+				},
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.solargraph.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>mn", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>rf", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
-  {
-    "rust-lang/rust.vim",
-    config = function()
-      vim.g.rustfmt_autosave = 1
-    end,
-  },
---  {
---    "sigmasd/deno-nvim",
---    config = function ()
---      local util = require("lspconfig/util")
---      require('deno-nvim').setup(
---        {
---          root_dir = util.root_pattern("mod.ts", "mod.js"),
---        });
---    end
---  },
-  --[[
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>mn", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>rf", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+	{
+		"rust-lang/rust.vim",
+		config = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
+	--  {
+	--    "sigmasd/deno-nvim",
+	--    config = function ()
+	--      local util = require("lspconfig/util")
+	--      require('deno-nvim').setup(
+	--        {
+	--          root_dir = util.root_pattern("mod.ts", "mod.js"),
+	--        });
+	--    end
+	--  },
+	--[[
   {
     "mrcjkb/rustaceanvim",
     version = "^4",
@@ -109,5 +112,5 @@ return {
     end,
   },
   ]]
-  --
+	--
 }
