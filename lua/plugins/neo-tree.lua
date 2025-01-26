831830a7e57f6180cc06ccb1e184532086d6b3e1
DevMobot <pro.dev.mohit@gmail.com>
2025-01-26 19:53:24 +0530

    update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,17 +5,10 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
-    "3rd/image.nvim",
+		"3rd/image.nvim",
 	},
 	config = function()
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-    require("neo-tree").setup({
-      window = {
-        mappings = {
-          ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } }
-        }
-      }
-    })
 	end,
 }
